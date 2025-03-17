b68a5d124fce5c2849c9cfb169ee219888498bee
nzkks <kkshanthosh@gmail.com>
2025-03-18 05:50:22 +1300

    getStreak utility function: fix getting the dateString

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,9 @@ import { Habit } from './store/habit-slice';
 export const getStreak = (habit: Habit) => {
   let streak = 0;
   const currentDate = new Date();
-  const dateString = currentDate.toISOString().split('T')[0];
 
   while (true) {
+    const dateString = currentDate.toISOString().split('T')[0];
     if (habit.completedDates.includes(dateString)) {
       streak++;
       currentDate.setDate(currentDate.getDate() - 1);

b0f6f1d753f0ae5050c42fd91536cbc7ee725720
nzkks <kkshanthosh@gmail.com>
2025-03-17 21:08:45 +1300

    App: style the title text

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Habit app using Vite + React + TS + Redux Toolkit + MUI
+# Habit Tracker app using Vite + React + TS + Redux Toolkit + MUI
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Provider } from 'react-redux';
+import { Container, Typography } from '@mui/material';
 
 import store from './store/store.ts';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>Habit app</div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h4" align="center">
+          Habit Tracker
+        </Typography>
+      </Container>
     </Provider>
   );
 }

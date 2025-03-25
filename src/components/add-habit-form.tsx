bfdabe08a0cfca019ce4515b763427f07fa76a80
nzkks <kkshanthosh@gmail.com>
2025-03-25 18:11:01 +1300

    Add Habit form: make use of Tanstack Form with Zod validation

--- src/components/add-habit-form.tsx
+++ src/components/add-habit-form.tsx
@@ -1,48 +1,94 @@
-import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { useForm } from '@tanstack/react-form';
+import { z } from 'zod';
+import { Box, Button, FormControl, FormHelperText, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 import { addHabit, HabitFrequency } from '../store/habit-slice';
 import { AppDispatch } from '../store/store';
 
+const habitSchema = z.object({
+  name: z.string().min(3, { message: 'Habit name must be at least 3 characters' }),
+  frequency: z.enum(['hourly', 'daily', 'weekly', 'fortnightly', 'monthly']),
+});
+
+type HabitSchema = z.infer<typeof habitSchema>;
+
 const AddHabitForm: React.FC = () => {
-  const [name, setName] = React.useState<string>('');
-  const [frequency, setFrequency] = React.useState<HabitFrequency>('daily');
   const dispatch = useDispatch<AppDispatch>();
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (name.trim()) {
-      dispatch(addHabit({ name, frequency }));
-      setName('');
-    }
-  };
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      frequency: 'daily',
+    } as HabitSchema,
+    validators: { onChange: habitSchema },
+    onSubmit: async ({ value }) => {
+      dispatch(addHabit(value));
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        e.stopPropagation();
+        form.handleSubmit();
+      }}
+    >
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-        <TextField
-          label="Habit Name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          placeholder="Write your habit name here"
+        <form.Field
+          name="name"
+          validators={{ onChangeAsyncDebounceMs: 500 }}
+          children={({ state, handleChange, handleBlur }) => {
+            return (
+              <TextField
+                label="Habit Name"
+                value={state.value}
+                onBlur={handleBlur}
+                onChange={e => handleChange(e.target.value)}
+                placeholder="Write your habit name here"
+                error={state.meta.isTouched && state.meta.errors.length > 0}
+                // helperText={state.meta.isTouched && state.meta.errors.join(', ')}
+              />
+            );
+          }}
+        />
+        <form.Field
+          name="frequency"
+          children={({ state, handleChange }) => (
+            <>
+              <FormControl fullWidth>
+                <InputLabel>Frequency</InputLabel>
+                <Select
+                  value={state.value}
+                  label="Frequency"
+                  onChange={e => handleChange(e.target.value as HabitFrequency)}
+                >
+                  <MenuItem value="hourly">Hourly</MenuItem>
+                  <MenuItem value="daily">Daily</MenuItem>
+                  <MenuItem value="weekly">Weekly</MenuItem>
+                  <MenuItem value="fortnightly">Fortnightly</MenuItem>
+                  <MenuItem value="monthly">Monthly</MenuItem>
+                </Select>
+                <FormHelperText>{state.meta.isTouched && state.meta.errors.join(', ')}</FormHelperText>
+              </FormControl>
+            </>
+          )}
         />
 
-        <FormControl fullWidth>
-          <InputLabel>Frequency</InputLabel>
-          <Select value={frequency} label="Frequency" onChange={e => setFrequency(e.target.value as HabitFrequency)}>
-            <MenuItem value="hourly">Hourly</MenuItem>
-            <MenuItem value="daily">Daily</MenuItem>
-            <MenuItem value="weekly">Weekly</MenuItem>
-            <MenuItem value="fortnightly">Fortnightly</MenuItem>
-            <MenuItem value="monthly">Monthly</MenuItem>
-          </Select>
-        </FormControl>
-
-        <Button variant="contained" type="submit" color="primary">
-          Add Habit
-        </Button>
+        <form.Subscribe
+          selector={state => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Box sx={{ display: 'flex', gap: 2 }}>
+              <Button variant="contained" type="submit" color="primary" disabled={!canSubmit}>
+                {isSubmitting ? '...' : 'Add Habit'}
+              </Button>
+              <Button variant="contained" type="reset" color="warning" onClick={() => form.reset()}>
+                Reset
+              </Button>
+            </Box>
+          )}
+        />
       </Box>
     </form>
   );

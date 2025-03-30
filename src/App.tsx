544ace6c11f08f9a0c0e3aef702392ab109c0e0a
nzkks <kkshanthosh@gmail.com>
2025-03-31 12:38:13 +1300

    Store: configure store options if any

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 
-import store from './app/store.ts';
+import { store } from './app/store.ts';
 import HabitTracker from './features/habit/habit-tracker.tsx';
 
 function App() {
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
 
 import habitsReducer from '../features/habit/habit-slice';
 
-const store = configureStore({
-  reducer: {
-    habits: habitsReducer,
-  },
-});
+export const createStore = (options?: ConfigureStoreOptions['preloadedState'] | undefined) =>
+  configureStore({
+    reducer: {
+      habits: habitsReducer,
+    },
+    ...options,
+  });
+
+export const store = createStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;

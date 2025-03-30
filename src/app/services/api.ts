14a58e4c93798d0e957299986590bb9a2e09218d
nzkks <kkshanthosh@gmail.com>
2025-03-31 12:43:00 +1300

    create api service using Redux Toolkit Query and add in the store

---
+++ src/app/services/api.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+
+const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:3001' });
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 6 });
+
+export const api = createApi({
+  reducerPath: 'splitApi',
+  baseQuery: baseQueryWithRetry,
+  endpoints: () => ({}),
+});
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,15 @@
 import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
 
+import { api } from './services/api';
 import habitsReducer from '../features/habit/habit-slice';
 
 export const createStore = (options?: ConfigureStoreOptions['preloadedState'] | undefined) =>
   configureStore({
     reducer: {
+      [api.reducerPath]: api.reducer,
       habits: habitsReducer,
     },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
     ...options,
   });
 
